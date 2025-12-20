a60178f1008afc6be4cdfdd55f050ddab32e8251
Sarthak Dudhe <sarthakdudhe79@gmail.com>
2025-12-20 14:37:58 +0530

    Update frontend URL for order placement

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -9,7 +9,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 //Placing user Order from frontend
 const placeOrder = async (req,res) => {
 
-const frontend_url = "http://localhost:5174"
+const frontend_url = "https://feasto-food-delivery-platform.onrender.com"
 
   try {
     const newOrder = new orderModel({
